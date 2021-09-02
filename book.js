5516688715d327bfbfb1dd62fa399e5288ac1631
QNabila <41321919+QNabila@users.noreply.github.com>
2021-09-02 17:01:42 +0600

    edition

--- book.js
+++ book.js
@@ -1,53 +1,58 @@
+// alert for no input
+document.getElementById("alert").style.display = "none";
 // Get input
 const searchBook = () => {
-    const searchField = document.getElementById("search-field");
-    const searchText = searchField.value;
-
-    
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  // alert for no input
+  if (searchField.value == 0) {
+    document.getElementById("alert").style.display = "block";
+  } else {
     // clear serach field
-    searchField.value = '';
+    searchField.value = "";
 
     //fetch data
     fetch(`https://openlibrary.org/search.json?q=${searchText}`)
-        .then((res) => res.json())
-        .then((data) => displayBook(data.docs,data));
+      .then((res) => res.json())
+      .then((data) => displayBook(data.docs, data));
 
-    
     // Display books on ui
-    const displayBook = (books, data) => {
-        const totalResults = document.getElementById('total-results');
-        // clear result found text
-        totalResults.textContent = '';
-        const resultDiv = document.createElement('div');
+    const displayBook = (resultBooks, data) => {
+      const totalResults = document.getElementById("total-results");
+      // clear result found text
+      totalResults.textContent = "";
+      const resultDiv = document.createElement("div");
 
-        // Check wheather input data is available
-        if (data.numFound) {
-            resultDiv.innerHTML = `<h2>${data.numFound} number of results found </h2>`;
+      // Check wheather input data is available
+      if (data.numFound) {
+        resultDiv.innerHTML = `<h4 class="text-center mb-4">${data.numFound} number of results found </h4>`;
         totalResults.appendChild(resultDiv);
-        }
-        else {
-            resultDiv.innerHTML = `<h2>No results found </h2>`;
+      } else {
+        resultDiv.innerHTML = `<h2 class="text-center">No results found </h2>`;
         totalResults.appendChild(resultDiv);
-        }
+      }
 
-        const searchResult = document.getElementById('search-result');
-        // clear previous result
-        searchResult.innerText = '';
-        books.forEach(book => {
-            console.log(book);
-            if (book.publisher == undefined) {
-                book.publisher = 'Not published';
-            }
-            if (book.author_name == undefined) {
-                book.author_name = 'Not published';
-            }
-            if (book.first_publish_year == undefined) {
-                book.first_publish_year = 'Not published';
-            }
-            const div = document.createElement('div');
-          
-                div.innerHTML = ` <div class="col">
-                <div class="card">
+      const searchResult = document.getElementById("search-result");
+      // clear previous result
+      searchResult.innerText = "";
+      // limiting searching result
+      const books = resultBooks.slice(0, 20);
+      books.forEach((book) => {
+        console.log(book);
+        // assigning  value for absent informations
+        if (book.publisher == undefined) {
+          book.publisher = "Not published";
+        }
+        if (book.author_name == undefined) {
+          book.author_name = "Not published";
+        }
+        if (book.first_publish_year == undefined) {
+          book.first_publish_year = "Not published";
+        }
+        const div = document.createElement("div");
+
+        div.innerHTML = `<div class="col">
+                <div class="card h-75">
                 <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"class="card-img-top" alt="...">
                   <div class="card-body">
                     <h5 class="card-title">Book Title:${book.title}</h5>
@@ -57,8 +62,8 @@ const searchBook = () => {
                   </div>
                 </div>
               </div>`;
-                searchResult.appendChild(div);
-            
-        })
-    }
+        searchResult.appendChild(div);
+      });
+    };
+  }
 };
